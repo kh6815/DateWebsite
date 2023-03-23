7799f73199341677bd84ed094e3173dc1f204b32
Choi kangHyeon <khchoi@firstclabs.com>
2023-03-23 16:14:01 +0900

    spring server 세팅

---
+++ DateWebsite Spring Server/src/main/java/com/date/DateWebsite/DateWebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.date.DateWebsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DateWebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DateWebsiteApplication.class, args);
+	}
+
+}
